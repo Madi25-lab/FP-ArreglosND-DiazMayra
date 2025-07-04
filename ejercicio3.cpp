8d8176e02fb9bd5937bf2f765c08a4636a2cb91a
Madi25-lab <Madic2507@gmail.com>
2025-07-04 00:21:09 -0500

    Agregando ejercicio 3, version final para la suma de la diagonal principal y secundaria de una matriz de 4*4

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 	int matriz[4][4];
-	int D1, D2;
+	int D1=0, D2=0;
 	
 	cout << "==========================================================" << endl;
     cout << "            SUMA DE DIAGONALES DE UNA MATRIZ (MD)" << endl;
@@ -26,6 +26,22 @@ int main(){
 		}
 		cout<<endl;
 	}
+	
+	cout<<endl;
+	
+	for(int i=0; i<4; i++){
+			D1=D1+matriz[i][3-i];
+			D2=D2+matriz[i][i];
+		}	
+	
+	cout<<"Suma Diagonal principal: "<<D1<<endl;
+	cout<<"Suma Diagonal secundaria: "<<D2<<endl;
+	cout<<endl;
+	
+	int T=0;
+	T=D1+D2;cout<<"Suma de diagonales: "<<T;
 
     return 0;	
-}
+		
+	}
+	
