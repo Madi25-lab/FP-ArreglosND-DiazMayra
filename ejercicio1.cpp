8ae4cda755b670503c9ee43b4b2358727c86b38f
Madi25-lab <Madic2507@gmail.com>
2025-07-03 22:30:09 -0500

    Agregando ejercicio 1, version final para el producto de 2 matrices de 3*3

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main (){
 	int matriz1 [3][3];
 	int matriz2 [3][3];
+	int producto [3][3];
 	
 	cout << "==========================================================" << endl;
     cout << "            PRODUCTO DE MATRICES DE 3X3" << endl;
@@ -21,7 +22,7 @@ int main (){
 	
 	cout<<endl;
 	
-	for (int i=0; i<3; i++){
+	for (int i=0; i<3; i++){   // Inicializar la matriz producto con ceros
 		for (int j=0; j<3; j++){
 			cout<<"Ingrese el valor de la matriz 2 [ "<<i<<"]["<<j<<"]:  ";
 			cin>>matriz2[i][j];
@@ -29,19 +30,40 @@ int main (){
 	}
 	system ("cls");
 	
-	cout<<"---------------------------PRODUCTO--------------------------"<<endl;
+	for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            producto[i][j] = 0;
+        }
+    }
 	
+	cout<<"---------------------------PRODUCTO--------------------------"<<endl;
+	cout<<endl;
+	
+	for (int i=0; i<3; i++){
+	    for(int j=0; j<3; j++){
+			for(int k=0; k<3; k++){
+				producto[i][j]+=matriz1[i][k]*matriz2[k][j];
+			}
+		}
+	}
+			
 	for (int i=0; i<3; i++){
 		for (int j=0; j<3; j++){
 			cout<<matriz1[i][j]<<" ";
 		}
+	if (i==1) cout<<"+ ";
+	else cout<<"\t";
+	for (int j=0; j<3; j++){
+		cout<<matriz2[i][j]<<" ";
+	    }	
+	if (i==1) cout<<"= ";
+	else cout<<"\t";	
+	for (int j=0; j<3; j++){
+		cout<<producto[i][j]<<" ";
+		}
 		
-	cout<<"   ";
+	cout<<endl;	
+	}	
 	
-		for (int j=0; j<3; j++){
-			cout<<matriz2[i][j]<<" ";
-		}	
-		cout<<endl;
-	}
 	return 0;
 }
