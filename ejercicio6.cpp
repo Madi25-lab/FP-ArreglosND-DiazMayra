7de69e396a9c61b83f763261556c39de77263bb4
Madi25-lab <Madic2507@gmail.com>
2025-07-05 00:07:21 -0500

    Agregando ejercicio 6, version final para el traslado de un valor a una posicion mas 'matriz reflejada horizontalmente'

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
 	int matriz[6][6];
+	int matriz1 [6][6];
 	
 	cout << "==========================================================" << endl;
     cout << "              MATRIZ REFLEJADA (MD)" << endl;
@@ -18,15 +19,28 @@ int main(){
 	system ("cls");
 	
 	cout<<endl;	
-	cout<< "-------------------MATRIZ ORIGINAL------------------"<<endl;
+	cout<< "-----------MATRIZ REFLEJADA HORIZONTALMENTE----------"<<endl;
 	cout<<endl;
+	
+	for(int i=0; i<6; i++){
+		for(int j=0; j<6; j++){
+		    matriz1[i][j]=matriz[i][5-j];
+		}
+	}
 	
 	for(int i=0; i<6; i++){
 		for(int j=0; j<6; j++){
 			cout<<matriz[i][j]<<" ";
 		}
+		
+		cout<<"     ";
+		
+		for(int j=0; j<6; j++){
+		    cout<<matriz1[i][j]<<" ";
+		}	
 		cout<<endl;
 	}
+
 	
 	return 0;
 }
