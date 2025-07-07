3a40adcbd4ccf555af7304a56e8bcdfe22741f16
Madi25-lab <Madic2507@gmail.com>
2025-07-06 23:25:16 -0500

    Agregando ejercicio 11, version final para la realizacion del juego de 'P.de fuego' donde se verifica el valor de la celda y su alrededor de ella

--- ejercicio11.cpp
+++ ejercicio11.cpp
@@ -1,34 +1,70 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
 #include<ctime>
-
 using namespace std;
 
-int main(){
-	cout << "==========================================================" << endl;
+int main(){	
+const int n = 10;
+
+    cout << "==========================================================" << endl;
     cout << "                 PROPAGACION DE FUEGO (MD)" << endl;
     cout << "==========================================================" << endl;
 	cout<<endl;
 	
 	srand(time(0));
-	int matriz[10][10];
-	int P_fuego[10][10];
-	int S_fuego[10][10];
-	
-	srand(time(0));
-	
+	int bosque[10][10];
+    int P_fuego[10][10]; 
+    
 	for(int i=0; i<10; i++){
 		for(int j=0; j<10; j++){
-			matriz[i][j]=rand()%3;
+			bosque[i][j]=rand()%3;
 		}
 	}
-	
-	for(int i=0; i<10; i++){
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(bosque[i][j]==0){ 
+                bool C_quemado=false;
+                for(int k=0; k<8; k++){
+				int fila[]={-1,-1,-1,0,0,1,1,1};
+				int columna[]={-1, 0, 1,-1,1,-1,0,1};
+				int NF=i+fila[k];
+				int NC=j+columna[k];
+				    if(NF>=0 && NF<10 && NC>=0 && NC<10){
+                            if (bosque[NF][NC]==1){
+                                C_quemado=true;
+                            }
+                        }
+                    }
+                if(C_quemado){
+				    P_fuego[i][j]=1;
+				}else{
+				    P_fuego[i][j]=0;
+				} 
+            }else if(bosque[i][j]==1){
+                P_fuego[i][j]=2;
+            }else{
+                P_fuego[i][j]=2;
+            }
+        }
+    }
+    
+    cout<<"------------------------RESULTADO-----------------------"<<endl;
+    cout<<endl;
+
+    for(int i=0; i<10; i++){
 		for(int j=0; j<10; j++){
-			cout<<matriz[i][j]<<" ";
+			cout<<bosque[i][j]<<" ";
 		}
+	
+	cout<<"                  ";
+	     
+	    for(int j=0; j<10; j++){
+			cout<<P_fuego[i][j]<<" ";
+		}
+	     
 		cout<<endl;
 	}
 	
-return 0;
+	return 0;
 }
